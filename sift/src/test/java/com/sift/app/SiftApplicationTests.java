94e177de1e258c36ae6ccc799f4f267fe0649853
Daniel <danielcoblentz916@gmail.com>
2025-06-15 22:16:15 -0700

    first commit/ starting point

---
+++ sift/src/test/java/com/sift/app/SiftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sift.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
